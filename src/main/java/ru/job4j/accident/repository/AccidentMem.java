3722d61287a15b7a666569365229e708fa0a8d02
mikha <mixa199525@yandex.ru>
2021-12-23 19:34:14 +0300

    2. IndexControl. Таблица и вид. [#2092 #242376]

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -2,8 +2,6 @@ package ru.job4j.accident.repository;
 
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
-import ru.job4j.accident.service.AccidentService;
-
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
