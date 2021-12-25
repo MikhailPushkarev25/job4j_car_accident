1e154887b53ad249e64e4f6068c0f1394ce1dd48
mikha <mixa199525@yandex.ru>
2021-12-25 19:07:55 +0300

    4. Form с композиционным объектом [#305522]

--- src/main/java/ru/job4j/accident/model/Accident.java
+++ src/main/java/ru/job4j/accident/model/Accident.java
@@ -13,14 +13,6 @@ public class Accident {
     public Accident() {
     }
 
-//    public Accident(int id, String name, String text, String address, AccidentType type) {
-//        this.id = id;
-//        this.name = name;
-//        this.text = text;
-//        this.address = address;
-//        this.type = type;
-//    }
-
     public Accident(String name, String text, String address, AccidentType type) {
         this.name = name;
         this.text = text;
