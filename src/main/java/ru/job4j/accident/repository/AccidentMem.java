3f80488153159cbe49cbcf5e78a562540dae18fe
mikha <mixa199525@yandex.ru>
2022-01-10 22:50:37 +0300

    1. Spring ORM [#2093]

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -21,28 +21,28 @@ public class AccidentMem {
     private final AtomicInteger sizeRule = new AtomicInteger(4);
 
     public AccidentMem() {
-//        AccidentType type1 = AccidentType.of(1, "Две машины");
-//        AccidentType type2 = AccidentType.of(2, "Машина и человек");
-//        AccidentType type3 = AccidentType.of(3, "Машина и велосипед");
-//
-//        Set<Rule> rules1 = Set.of(Rule.of(1, "Статья 1"));
-//        Set<Rule> rules2 = Set.of(Rule.of(2, "Статья 2"));
-//        Set<Rule> rules3 = Set.of(Rule.of(3, "Статья 3"));
-//
-////        accidents.put(1, new Accident(
-////                 "Михаил", "Превышение скорости", "пр-кт Ленина 32", type1, rules1));
-////        accidents.put(2, new Accident(
-////                 "Роман", "Не пропустил пешехода", "ул. Баумана 100", type2, rules2));
-////        accidents.put(3, new Accident(
-////                "Василий", "совершил ДТП", "пр-скт Гагарина 35", type3, rules3));
-//
-//        typeMap.put(1,  AccidentType.of(1, "Две машины"));
-//        typeMap.put(2,  AccidentType.of(2, "Машина и человек"));
-//        typeMap.put(3,  AccidentType.of(3, "Машина и велосипед"));
-//
-//        ruleMap.put(1,  Rule.of(1, "Статья 1"));
-//        ruleMap.put(2,  Rule.of(2, "Статья 2"));
-//        ruleMap.put(3,  Rule.of(3, "Статья 3"));
+        AccidentType type1 = AccidentType.of(1, "Две машины");
+        AccidentType type2 = AccidentType.of(2, "Машина и человек");
+        AccidentType type3 = AccidentType.of(3, "Машина и велосипед");
+
+        Set<Rule> rules1 = Set.of(Rule.of("Статья 1"));
+        Set<Rule> rules2 = Set.of(Rule.of("Статья 2"));
+        Set<Rule> rules3 = Set.of(Rule.of("Статья 3"));
+
+        accidents.put(1, new Accident(1,
+                 "Михаил", "Превышение скорости", "пр-кт Ленина 32", type1, rules1));
+        accidents.put(2, new Accident(2,
+                 "Роман", "Не пропустил пешехода", "ул. Баумана 100", type2, rules2));
+        accidents.put(3, new Accident(3,
+                "Василий", "совершил ДТП", "пр-скт Гагарина 35", type3, rules3));
+
+        typeMap.put(1,  AccidentType.of(1, "Две машины"));
+        typeMap.put(2,  AccidentType.of(2, "Машина и человек"));
+        typeMap.put(3,  AccidentType.of(3, "Машина и велосипед"));
+
+        ruleMap.put(1,  Rule.of("Статья 1"));
+        ruleMap.put(2,  Rule.of("Статья 2"));
+        ruleMap.put(3,  Rule.of("Статья 3"));
     }
 
     public Collection<AccidentType> getValue() {
